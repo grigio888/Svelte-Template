90690a8e94e3175b79d870554d30aa1e69de6715
vinicius.grigio <vinicius.grigio@devoteam.com>
2025-08-04 11:22:12 +0100

    fix(Toaster): Update shadow color for improved visual consistency

--- src/components/sonner/Toaster.svelte
+++ src/components/sonner/Toaster.svelte
@@ -9,7 +9,7 @@
 		classes: {
 			toast: `min-h-16 w-90 p-4 flex gap-2 
             text-sm
-            bg-(--color-bg-100) shadow-xl shadow-(color:--color-theme-500)
+            bg-(--color-bg-100) shadow-xl shadow-(color:--color-theme-200)
             border border-(--border-color) rounded-md`,
 
 			error: 'text-red-600',
