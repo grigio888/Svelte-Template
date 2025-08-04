aef5a6a4313603ea055bd4735d2ef5824912bbf0
vinicius.grigio <vinicius.grigio@devoteam.com>
2025-08-04 17:49:46 +0100

    fix(ErrorPage): Update shadow and background colors for improved visibility

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -17,11 +17,11 @@
 	<div
 		class="relative flex min-h-60 w-[90%] flex-col items-center justify-center gap-10
         rounded-md border border-(--border-color) p-12 pt-16 shadow-xl
-        shadow-(color:--color-theme-800) md:max-w-80"
+        shadow-(color:--color-theme-300) md:max-w-80"
 	>
 		<h1
 			class="absolute top-0 left-1/2 translate-x-[-50%] -translate-y-1/2 transform
-            bg-(--color-bg-950) px-3 text-center text-6xl font-semibold"
+            bg-(--color-bg-100) px-3 text-center text-6xl font-semibold"
 		>
 			{$page.status}
 		</h1>
