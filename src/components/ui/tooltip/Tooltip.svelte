e34dd0c7c34f7a0757ccd3af4b8e779393d9cdd8
vinicius.grigio <vinicius.grigio@devoteam.com>
2025-08-04 11:21:52 +0100

    feat(Components): Improved Tooltip behavior and colors.

--- src/components/ui/tooltip/Tooltip.svelte
+++ src/components/ui/tooltip/Tooltip.svelte
@@ -1,28 +1,38 @@
 <script lang="ts">
 	// »»» Props
-	let props = $props();
+	let {
+        message = '',
+        class: classProp = '',
+        children,
+    } = $props();
 
 	// »»» Components
 	import { Tooltip } from 'bits-ui';
+
+    // »»» Logic
+    let defaultWrapperClass = `relative transition-background ring-(--color-bg-300) ring-offset-background
+        inline-flex size-10! items-center justify-center rounded-full border border-(--color-theme-300)
+        bg-(--color-bg-100) duration-300 hover:bg-(--color-bg-300) focus-visible:ring-2
+        focus-visible:ring-offset-2 focus-visible:outline-hidden ${classProp}
+    `;
+    let defaultTooltipClass = `z-0 flex items-center justify-center rounded-md
+        border border-(--color-theme-300) bg-(--color-bg-100) p-3 text-sm shadow-lg
+        shadow-(color:--color-theme-300) outline-hidden
+    `;
 </script>
 
 <Tooltip.Provider>
 	<Tooltip.Root delayDuration={200}>
 		<Tooltip.Trigger
-			class="transition-background ring-dark ring-offset-background inline-flex size-10 items-center
-            justify-center rounded-full border border-(--border-color) bg-(--color-bg-950) duration-300
-            hover:bg-(--color-bg-900) focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden
-            {props.class}"
+			class={classProp != 'none' ? defaultWrapperClass : 'w-full'}
 		>
-			{@render props.children()}
+            {@render children()}
 		</Tooltip.Trigger>
-		<Tooltip.Content sideOffset={8}>
+		<Tooltip.Content sideOffset={10}>
 			<div
-				class="z-0 flex items-center justify-center rounded-md
-                border border-(--border-color) bg-(--color-bg-950) p-3 text-sm font-medium shadow-lg
-                shadow-purple-700/50 outline-hidden"
+                class={message ? defaultTooltipClass : 'hidden'}
 			>
-				{props.message}
+				{message}
 			</div>
 		</Tooltip.Content>
 	</Tooltip.Root>
