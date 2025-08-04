c48ab8f0e7eed718c23135659fe813f2730050e4
vinicius.grigio <vinicius.grigio@devoteam.com>
2025-08-04 17:50:01 +0100

    feat(layout): Add Background component to layout for improved UI structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,18 +4,25 @@
 
 	import { PUBLIC_ENVIRONMENT } from '$env/static/public';
 	import { locale } from '$i18n/store.js';
+    import { toast } from 'svelte-sonner';
+    import { goto } from '$app/navigation';
+    import { translate as _ } from '$i18n/translate';
+    import { page } from '$app/stores';
 
 	// »»» Props
 	let { data, children } = $props();
 
 	// »»» Components
 	import PageTransition from '$comp/ui/PageTransition.svelte';
+	import Background from '$comp/ui/background/Background.svelte';
 	import Toaster from '$comp/sonner/Toaster.svelte';
 
 	// »»» Logic
 	let language = $state(data?.lang);
 	locale.set(language);
 </script>
+
+<Background />
 
 <PageTransition refresh={data?.url}>
 	{@render children()}
