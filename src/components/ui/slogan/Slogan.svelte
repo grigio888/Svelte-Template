4a1e55957f038a3b04fcd06283231b1731d7cecb
vinicius.grigio <vinicius.grigio@devoteam.com>
2025-08-04 11:23:25 +0100

    feat(Slogan): Implement dynamic slogan splitting and translation

--- src/components/ui/slogan/Slogan.svelte
+++ src/components/ui/slogan/Slogan.svelte
@@ -1,3 +1,17 @@
-<h1 class="w-full text-center text-6xl font-bold">
-	Home <span class="text-(--color-theme-600)">Page</span>
+<script>
+	// »»» Imports
+	import { translate as _ } from '$i18n/translate';
+
+    let name = _("Site's Name");
+
+    let words = name.split(' ');
+    let splitIndex = Math.ceil(words.length / 2);
+    if (words.length === 3) splitIndex = 2;
+    if (words.length === 5) splitIndex = 3;
+    let firstPart = words.slice(0, splitIndex).join(' ');
+    let secondPart = words.slice(splitIndex).join(' ');
+</script>
+
+<h1 class="w-full text-center text-4xl md:text-6xl font-bold">
+	{firstPart} <span class="text-(--color-theme-600)">{secondPart}</span>
 </h1>
